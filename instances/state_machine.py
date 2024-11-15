64673593ccda7e086a96bf27536a1da9685f9572
Reno Greenleaf <reno.greenleaf@gmail.com>
2024-11-15 16:01:20 +0300

    State machine isn't hardcoded anymore.

--- instances/state_machine.py
+++ instances/state_machine.py
@@ -1,16 +1,15 @@
 from instances.events import events
 from instances.scenes import scenes
+from json import load
 
+transitions = []
 
-transitions = [
-	(events['scene.asked_for_menu'], scenes['menu']),
-	(events['scene.decided_to_exit'], scenes['exit']),
-	(events['scene.asked_for_help'], scenes['help']),
-	(events['menu.coinflip_selected'], scenes['coin_flip']),
-	(events['menu.lockpick_selected'], scenes['lockpick']),
-	(events['menu.exploration_selected'], scenes['room']),
-	(events['menu.help_selected'], scenes['help']),
-]
-current_scene = scenes['menu']
-previous_scene_available = events['state_machine.previous_scene_available']
-previous_requested = events['state_machine.previous_requested']
+with open('instances/state_machine.json', 'r') as sm_json:
+	sm_data = load(sm_json)
+
+	for event_name, scene_name in sm_data['transitions'].items():
+		transitions.append((events[event_name], scenes[scene_name]))
+
+	current_scene = scenes[sm_data['current_scene']]
+	previous_scene_available = events[sm_data['previous_scene_available']]
+	previous_requested = events[sm_data['previous_requested']]
