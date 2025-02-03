d878dc7b7ca1cf7cad21bc0095ffc72ef8ce7fe7
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-02-03 17:14:08 +0300

    Config adjustment.

--- settings.py
+++ settings.py
@@ -2,13 +2,8 @@ TORTOISE = {
     "connections": {
         "default": "sqlite://db.sqlite3"
     },
-    "apps": {
-        "models": {
-            "models": [
-                "aerich.models",
-                "ending.models"
-            ],
-            "default_connection": "default"
-        }
-    }
+    "apps": {"models": {"models": [
+        "aerich.models",
+        "ending.models"
+    ]}}
 }
