0c800339579dc562c4c5ee12c15237db89c304b1
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-02-06 19:17:11 +0300

    Event column.

--- event/models.py
+++ event/models.py
@@ -1,7 +1,10 @@
-from tortoise.models import Model
-from tortoise import fields
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import mapped_column
+from reusables.model import Model
 
 
 class Event(Model):
-	id = fields.IntField(primary_key=True)
-	name = fields.CharField(max_length=255)
+	__tablename__ = 'event'
+
+	id = mapped_column(Integer(), primary_key=True)
+	name = mapped_column(String(255), nullable=False)
