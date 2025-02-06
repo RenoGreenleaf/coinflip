6475faa7588bc39fd855a255425c207fa97d1d69
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-02-06 18:21:07 +0300

    Documentation for reusables.

---
+++ reusables/__init__.py
@@ -0,0 +1,5 @@
+"""
+Contains abstract classes or helper functions.
+As the name suggests the purpose of it is to provide common code pieces to be
+used throughout the whole project. It doesn't represent an entity type.
+"""
