f973219c90590c69e862f08e192d94e14e9020a2
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-02-17 18:17:38 +0300

    Renaming of an exit.

--- room/editor.py
+++ room/editor.py
@@ -50,10 +50,22 @@ class Editor(Cmd):
 			session.add(self.model)
 			session.commit()
 
-	def complete_delete(self, text, line, begidx, endidx):
-		return self._exits_names_complete(text)
+	def do_rename(self, args):
+		new_name = input("New name: ")
 
-	def _exits_names_complete(self, text):
+		with Session() as session:
+			exit_ = self.model.find_exit_by_name(args)
+			exit_.name = new_name
+			session.add(exit_)
+			session.commit()
+
+	def complete_delete(self, text, line, begidx, endidx):
+		return self._exits_names_autocomplete(text)
+
+	def complete_rename(self, text, line, begidx, endidx):
+		return self._exits_names_autocomplete(text)
+
+	def _exits_names_autocomplete(self, text):
 		exits = self.model.find_exits(text)
 		return [exit_.name for exit_ in exits]
 
