de3b00c892fd6313dae317d9ed465c337344857e
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-02-20 15:41:48 +0300

    Changes to pool objects in memory aren't overwritten by loading'em from DB.

--- instances/pool.py
+++ instances/pool.py
@@ -8,21 +8,29 @@ from instances.editor import Editor
 class Pool:
 	def __init__(self):
 		self.editor = None
+		self.scenes = []
+		self.events = []
 
 	def get_all_events(self):
 		with self.get_db_session() as session:
-			return session.scalars(select(Event)).all()
+			session.add_all(self.events)
+			self.events = session.scalars(select(Event)).all()
+			return self.events
 
 	def get_all_scenes(self):
 		with self.get_db_session() as session:
-			return session.scalars(select(Scene)).all()
+			session.add_all(self.scenes)
+			self.scenes = session.scalars(select(Scene)).all()
+			return self.scenes
 
 	def get_event(self, identifier):
 		with self.get_db_session() as session:
+			session.add_all(self.scenes)
 			return session.get(Event, identifier)
 
 	def get_scene(self, identifier):
 		with self.get_db_session() as session:
+			session.add_all(self.scenes)
 			return session.get(Scene, identifier)
 
 	def wrap_for_editing(self, pool):
