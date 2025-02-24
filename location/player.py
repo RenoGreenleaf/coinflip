c2173145f71f332f1ed54ae5760689ed3469faa8
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-02-24 14:50:15 +0300

    Looking around in a location.

--- location/player.py
+++ location/player.py
@@ -2,6 +2,7 @@ from cmd2 import Cmd
 
 
 class Player(Cmd):
+	prompt = "> "
 	def __init__(self, location, pool):
 		super().__init__()
 		self.model = location
@@ -9,3 +10,15 @@ class Player(Cmd):
 
 	def interact(self):
 		self.cmdloop()
+
+	def do_look(self, args):
+		if args != 'around':
+			print("Unclear.")
+			return
+
+		with self.pool.get_db_session() as session:
+			session.add(self.model)
+			print(self.model.description)
+
+	def complete_look(self, text, line, begidx, endidx):
+		return ['around']
