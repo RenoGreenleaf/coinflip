913e567d38bdf0d9f4b94864a1973f25bc321627
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-03-05 17:49:21 +0300

    "look at" instead of "examine".

--- location/player.py
+++ location/player.py
@@ -1,8 +1,29 @@
-from cmd2 import Cmd
+from cmd2 import Cmd, Cmd2ArgumentParser, with_argparser
 
 
 class Player(Cmd):
 	prompt = "> "
+
+	def look_around(self, args):
+		print("Looking around.")
+		print(self.model.description)
+
+	def look_at(self, args):
+		print(f"Looking at {args.exit}.")
+		exit_ = self.model.find_exit_by_name(args.exit)
+		print(exit_.description)
+
+	def look_at_choices(self):
+		return [exit_.name for exit_ in self.model.find_exits('')]
+
+	# look at/around
+	look_parser = Cmd2ArgumentParser()
+	look_subparsers = look_parser.add_subparsers()
+	at_parser = look_subparsers.add_parser('at')
+	at_parser.add_argument('exit', choices_provider=look_at_choices)
+	around_parser = look_subparsers.add_parser('around')
+	at_parser.set_defaults(func=look_at)
+	around_parser.set_defaults(func=look_around)
 
 	def __init__(self, location, pool):
 		super().__init__()
@@ -12,20 +33,18 @@ class Player(Cmd):
 	def interact(self):
 		self.cmdloop()
 
+	@with_argparser(look_parser)
 	def do_look(self, args):
-		if args != 'around':
-			print("Unclear.")
-			return
+		function = getattr(args, 'func', None)
 
-		print(self.model.description)
+		if function:
+			function(self, args)
+		else:
+			print("Unclear.")
 
 	def do_leave(self, args):
 		self.model.discovered_event.trigger()
 		return True
-
-	def do_examine(self, args):
-		exit_ = self.model.find_exit_by_name(args.strip('"'))
-		print(exit_.description)
 
 	def do_use(self, args):
 		name = args.strip('"')
@@ -36,9 +55,3 @@ class Player(Cmd):
 
 	def complete_use(self, text, line, begidx, endidx):
 		return [exit_.name for exit_ in self.model.find_exits(text)]
-
-	def complete_examine(self, text, line, begidx, endidx):
-		return [exit_.name for exit_ in self.model.find_exits(text)]
-
-	def complete_look(self, text, line, begidx, endidx):
-		return ['around']
