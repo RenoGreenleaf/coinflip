92a51a7eb27dfe7a372bc1b494304d64d2188b04
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-06-10 19:47:50 +0300

    Default commands are removed.

--- play.py
+++ play.py
@@ -1,7 +1,20 @@
 #!/usr/bin/env python
+import cmd2
 from json import load
 from instances.pool import Pool
 
+
+del cmd2.Cmd.do_alias
+del cmd2.Cmd.do_edit
+del cmd2.Cmd.do_eof
+del cmd2.Cmd.do_ipy
+del cmd2.Cmd.do_py
+del cmd2.Cmd.do_quit
+del cmd2.Cmd.do_set
+del cmd2.Cmd.do_shell
+del cmd2.Cmd.do_shortcuts
+del cmd2.Cmd.do_run_pyscript
+del cmd2.Cmd.do_run_script
 
 pool = Pool()
 
