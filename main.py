c925bbcb9bd3f33ba8a491ca052367c58bb82a16
Reno Greenleaf <reno.greenleaf@gmail.com>
2025-08-10 18:56:45 +0300

    Removal action.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from types import MappingProxyType
 
 
 @dataclass
@@ -6,6 +7,14 @@ class Message:
 	"""It's meant to be sent to a world and be processed by it."""
 
 	text: str = ""
+	actions: dict = MappingProxyType({})
+
+	def save(self):
+		return {}
+
+	def load(self, json):
+		self.text = json['text']
+		self.actions = MappingProxyType(json['actions'])
 
 
 class World:
@@ -13,20 +22,17 @@ class World:
 
 	def get_option(self):
 		key = input("> ")
-
-		if key not in self.options:
-			option = Option()
-			option.load({'text': "Unclear", 'description': ""})
-			return option
-
-		return self.options[key]
+		return self.options.get(key, EmptyOption())
 
 	def process(self, message):
-		print("---------------")
-		print(message.text, "\n")
+		self._print("---------------")
+		self._print(message.text)
+		print()
+
+		self._remove(message)
 
 		for option in self.options.values():
-			print(option)
+			self._print(option)
 
 	def save(self):
 		return {}
@@ -39,19 +45,35 @@ class World:
 			option.load(raw_option)
 			self.options[key] = option
 
+	def _remove(self, message):
+		to_remove = message.actions.get('remove', [])
+
+		for key in to_remove:
+			self.options.pop(key)
+
+	def _print(self, text):
+		if text != "":
+			print(text)
+
 
 class Option:
 	"""Represents what can be done at the moment."""
 
 	def get_message(self):
-		return Message(text=self.text)
+		return self.message
 
 	def save(self):
 		return {}
 
 	def load(self, json):
-		self.text = json['text']
 		self.description = json['description']
+		self.message = Message()
+		self.message.load(json['message'])
 
 	def __repr__(self):
 		return self.description
+
+
+class EmptyOption:
+	def get_message(self):
+		return Message()
